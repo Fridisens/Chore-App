3363a2e4977580d8872bfb9fde803189925f5fb1
Fridisens <fridisens@gmail.com>
2025-02-17 11:19:08 +0100

    Initial Commit

---
+++ Chore-App/Chore_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chore_AppApp.swift
+//  Chore-App
+//
+//  Created by Frida Dahlqvist on 2025-02-17.
+//
+
+import SwiftUI
+
+@main
+struct Chore_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
